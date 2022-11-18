1dddbfb0fe180bcd87aa60b25aa7420e68ab0d8c
Lucas Gabriel <lucasgosilva17@gmail.com>
2022-11-18 16:49:35 -0300

    Date update

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.instagram.com/zlucasz17">@ zlucasz17</a>
                 </p>
             </div>
         </header>
